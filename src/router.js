696fa08850befdb11262574c109b502f520cec4a
Adefemi <adefemi.oseni@algorimsng.com>
2020-02-28 11:35:04 +0100

    updated

--- src/router.js
+++ src/router.js
@@ -4,32 +4,14 @@ import MainLayout from "./components/mainLayout/mainLayout";
 import PropertyListing from "./pages/propertyListing/propertyListing";
 import Properties from "./pages/properties/Properties";
 import Application from "./pages/application/Application";
-<<<<<<< HEAD
-import ProfileTemplate from "./pages/profile/ProfileTemplate";
-import Branch from "./pages/branch/Branch";
-=======
-import TenantInvite from "./pages/tenantInvite/TenantInvite";
->>>>>>> bd2ea5f33c33b7045bcf197e9541d5f8544ed045
 const HomeIndex = lazy(() => import("./pages/Dashboard/dashboard"));
 const TeamCreatePage = lazy(() => import("./pages/team/TeamCreatePage.js"));
 const TeamMembersPage = lazy(() => import("./pages/team/TeamMembersPage.js"));
-const AgencyPortfolio = lazy(() =>
-  import("./pages/agencyPortfolio/AgencyPortfolio")
-);
 
 const RouterMain = props => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route
-          path="/profile"
-          exact
-          component={props => (
-            <Suspense fallback={() => <h2>Loading...</h2>}>
-              <ProfileTemplate {...props} />
-            </Suspense>
-          )}
-        />
         <Route
           path="/"
           component={props => (
@@ -61,24 +43,7 @@ const RouterMain = props => {
                   </Suspense>
                 )}
               />
-              <Route
-                path="/tenant-invite"
-                exact
-                component={props => (
-                  <Suspense fallback={() => <h2>Loading...</h2>}>
-                    <TenantInvite {...props} />
-                  </Suspense>
-                )}
-              />
-              <Route
-                path="/agencies"
-                exact
-                component={props => (
-                  <Suspense fallback={() => <h2>Loading...</h2>}>
-                    <AgencyPortfolio {...props} />
-                  </Suspense>
-                )}
-              />
+
               <Route
                 path="/add-property"
                 exact
@@ -89,7 +54,6 @@ const RouterMain = props => {
                 )}
               />
 
-<<<<<<< HEAD
               <Route
                 path="/add-property/:uuid"
                 exact
@@ -99,8 +63,6 @@ const RouterMain = props => {
                   </Suspense>
                 )}
               />
-=======
->>>>>>> 23f558f4963cda780f152aa829aa253d54017818
               <Route
                 path="/properties"
                 exact
@@ -116,15 +78,6 @@ const RouterMain = props => {
                 component={props => (
                   <Suspense fallback={() => <h2>Loading...</h2>}>
                     <Application {...props} />
-                  </Suspense>
-                )}
-              />
-              <Route
-                path="/branch"
-                exact
-                component={props => (
-                  <Suspense fallback={() => <h2>Loading...</h2>}>
-                    <Branch {...props} />
                   </Suspense>
                 )}
               />
